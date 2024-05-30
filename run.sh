feb9826057a90343f697d1bba1e80c97463593b6
ishcha <isha4@illinois.edu>
2024-05-30 04:47:11 +0000

    scripts corrected

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 echo "starting with expt on vicuna 13b unk jb"
-python3 certification/main_hf_llms_regard.py 'unknown_jb_vicuna_13b_bold' 'unknown jb' --model_name 'lmsys/vicuna-13b-v1.5' > unknown_jb_vicuna_13b_bold.txt
+python3 certification/main_hf_llms_bold.py 'unknown_jb_vicuna_13b_bold' 'unknown jb' --model_name 'lmsys/vicuna-13b-v1.5' > unknown_jb_vicuna_13b_bold.txt
 
 echo "finished"
--- run_mistral.sh
+++ run_mistral.sh
@@ -1,4 +1,4 @@
 echo "starting with expt on Mistral-7b-instruct-v0.2 unk jb"
-python3 certification/main_hf_llms_regard.py 'unknown_jb_mistral_7b_bold' 'unknown jb' --model_name 'mistralai/Mistral-7B-Instruct-v0.2' >> results/unknown_jb_mistral_7b_bold.txt
+python3 certification/main_hf_llms_bold.py 'unknown_jb_mistral_7b_bold' 'unknown jb' --model_name 'mistralai/Mistral-7B-Instruct-v0.2' >> results/unknown_jb_mistral_7b_bold.txt
 
 echo "finished"
